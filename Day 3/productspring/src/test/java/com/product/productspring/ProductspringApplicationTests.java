d53a40f5d3c9182a258327f4b48ce9cd261b79cb
AdityaJoshiK <adityajoshi2107@gmail.com>
2023-05-22 16:16:00 +0530

    Spring Project Created

---
+++ Day 3/productspring/src/test/java/com/product/productspring/ProductspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.productspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
