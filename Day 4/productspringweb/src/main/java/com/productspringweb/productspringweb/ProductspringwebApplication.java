d3ba339392917259104118fba88f9d7185819478
AdityaJoshiK <adityajoshi2107@gmail.com>
2023-05-23 08:50:34 +0530

    Day 4 Project Created

---
+++ Day 4/productspringweb/src/main/java/com/productspringweb/productspringweb/ProductspringwebApplication.java
@@ -0,0 +1,13 @@
+package com.productspringweb.productspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductspringwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductspringwebApplication.class, args);
+	}
+
+}
---
+++ Day 4/productspringweb/src/test/java/com/productspringweb/productspringweb/ProductspringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productspringweb.productspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductspringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
